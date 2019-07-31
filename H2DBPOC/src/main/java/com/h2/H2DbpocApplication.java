ab4868fa00539ad1606f071f975a5e9a2f0441a4
realspeed <32242867+realspeed@users.noreply.github.com>
2019-07-31 08:49:27 +0530

    sd

--- H2DBPOC/src/main/java/com/h2/H2DbpocApplication.java
+++ H2DBPOC/src/main/java/com/h2/H2DbpocApplication.java
@@ -3,7 +3,7 @@ package com.h2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages="com.h2.*")
+@SpringBootApplication
 public class H2DbpocApplication {
 
 	public static void main(String[] args) {
